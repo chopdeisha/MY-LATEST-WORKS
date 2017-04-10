8bf8681f99a9f82f9ace96856ff6156bf4bdc7a7
Isha Chopde <isha.chopde@csu.fullerton.edu>
2017-04-10 10:06:06 -0700

    Add files via upload
    Updated student registration system code with a " class " named  Student and methods defined in the class.

--- student_registration/app.py
+++ student_registration/app.py
@@ -1,31 +1,34 @@
 student_list=[]
 
+class Student:
+    def __init__(self, name):
+        self.name = name
+        self.marks = []
+
+    def average_mark(self):
+        number = len(self.marks)
+        if number == 0:
+            return 0
+
+        total_sum = sum(self.marks)
+        return total_sum / number
+
 def create_student():
     # ask for student's name
     name = input("Enter new student's name: ")
     # create dictionary
-    student_data = {
-        "name": name,
-        "marks" :[]
-    }
+    student_data = Student(name)
     # return dictionary
     return student_data
 
 def add_mark(student, mark):
     # append marks to the dictionary
-    student["marks"].append(mark)
+    student.marks.append(mark)
 
-def calculate_average_marks(student):
-    number = len(student["marks"])
-    if number==0:
-        return 0
-
-    total_sum = sum(student["marks"])
-    return total_sum/number
 
 def student_details(student):
-    print("{},average marks: {}.".format(student["name"],
-                                        calculate_average_marks(student)))
+    print("{},average marks: {}.".format(student.name,
+                                        student.average_mark()))
 
 
 def print_student_list(students):
