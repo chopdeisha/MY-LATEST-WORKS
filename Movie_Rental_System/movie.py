cea1c286233c73b939a7817adc81129cb8e313b1
Isha Chopde <isha.chopde@csu.fullerton.edu>
2017-04-13 12:21:41 -0700

    MENU driven Movie rental system

--- Movie_Rental_System/movie.py
+++ Movie_Rental_System/movie.py
@@ -6,3 +6,15 @@ class Movie:
 
     def __repr__(self):
         return "<Movie {}>".format(self.name)
+
+    def json(self):
+        return {
+            "name": self.name,
+            "genre": self.genre,
+            "watched": self.watched
+
+        }
+
+    @classmethod
+    def load_from_json(cls,json_data):
+        return Movie(**json_data)
--- Movie_Rental_System/user.py
+++ Movie_Rental_System/user.py
@@ -16,27 +16,32 @@ class User:
         filter(lambda movie: movie.name != name, self.movies)
         #self.movies.delete(name)
 
+    def set_watched(self, name):
+        for movie in self.movies:
+            if movie.name == name:
+                movie.watched = True
+
+
     def movies_watched(self):
         # use filter method
         movies_watched = list(filter(lambda movie: movie.watched, self.movies))
         return movies_watched
 
-    def save_to_file(self):
-        with open("{}.txt".format(self.name), 'w') as f:
-            f.write(self.name + "\n")
-            for movie in self.movies:
-                f.write("{},{},{}".format(movie.name, movie.genre, movie.watched))
+    def json(self):
+        return {
+            'name': self.name,
+            'movie': [
+                movie.json() for movie in self.movies
+            ]
+        }
 
     @classmethod
-    def load_from_file(cls, filename):
-        with open(filename, 'r') as f:
-            content = f.readlines()
-            username = content[0]
-            movies = []
-            for line in content[1:]:
-                movie_data = line.split(",")
-                movies.append(Movie(movie_data[0],movie_data[1], movie_data[2] == "True"))
+    def load_from_json(cls, json_data):
+        user = User(json_data['name'])
+        movies =[]
+        for movie_data in json_data['movie']:
+            movies.append(Movie.load_from_json(movie_data))
+        user.movies = movies
 
-            user = User(username)
-            user.movies = movies
-            return user
+        return user
+
